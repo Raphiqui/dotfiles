826cb79b4de95bb7dfcaa554a6a4c37e7acc2174
Norsse <raphael.pastre@gmail.com>
2025-03-18 16:27:49 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -38,6 +38,8 @@ fi
 if ! command -v brew &> /dev/null; then
   echo "🍺 Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  # Load Homebrew into the current session
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 else
   echo "✅ Homebrew already installed!"
 fi
