3de09ad676eee0c4f224945ca59080ac8918440d
norsse <raphael.pastre@gmail.com>
2025-05-02 21:07:28 +0200

    Add prettier formatter

---
+++ dot_config/nvim/lua/plugins/null-ls.lua
@@ -0,0 +1,26 @@
+--[[
+--To add a plugin such as prettier make sure that you previously install it using Mason
+--]]
+
+return {
+  "nvimtools/none-ls.nvim",
+  opts = function(_, opts)
+    local null_ls = require("null-ls")
+
+    opts.sources = vim.list_extend(opts.sources or {}, {
+      null_ls.builtins.formatting.prettier.with({
+        filetypes = {
+          "javascript",
+          "typescript",
+          "javascriptreact",
+          "typescriptreact",
+          "vue",
+          "css",
+          "scss",
+          "html",
+          "json",
+        },
+      }),
+    })
+  end,
+}
